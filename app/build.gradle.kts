7cb069d40790677b1e9b63dae50cebbbb2aa740b
huynt2002 <uchitoyuki@gmail.com>
2025-04-01 17:47:04 +0700

    refactor build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,94 +1,97 @@
+import java.util.Properties
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.kotlin.plugin.serialization")
-    id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
-    id("com.google.gms.google-services")
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose)
+  id("org.jetbrains.kotlin.plugin.serialization")
+  id("com.google.devtools.ksp")
+  id("com.google.dagger.hilt.android")
+  id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.retrofit"
-    compileSdk = 35
+  namespace = "com.example.retrofit"
+  compileSdk = 35
 
-    defaultConfig {
-        applicationId = "com.example.retrofit"
-        minSdk = 29
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "com.example.retrofit"
+    minSdk = 29
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-        debug {
-            buildConfigField("String", "API_KEY", "")
-            buildConfigField("String", "WEB_CLIENT_KEY", "")
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+          getDefaultProguardFile("proguard-android-optimize.txt"),
+          "proguard-rules.pro",
+      )
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+    debug {
+      val localProperties =
+          Properties().apply { load(rootProject.file("local.properties").inputStream()) }
+
+      buildConfigField("String", "API_KEY", "\"${localProperties["API_KEY"]}\"")
+      buildConfigField("String", "WEB_CLIENT_KEY", "\"${localProperties["WEB_CLIENT_KEY"]}\"")
     }
-    kotlinOptions { jvmTarget = "11" }
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  kotlinOptions { jvmTarget = "11" }
+  buildFeatures {
+    compose = true
+    buildConfig = true
+  }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.graphics)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.material3)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.ui.test.junit4)
+  debugImplementation(libs.androidx.ui.tooling)
+  debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation(project(":auth_android:auth_kit"))
-    implementation(project(":android_ui_kit"))
+  implementation(project(":auth_android:auth_kit"))
+  implementation(project(":android_ui_kit"))
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
-    implementation("androidx.navigation:navigation-common:2.8.8")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+  implementation("androidx.navigation:navigation-common:2.8.8")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.3")
 
-    ksp("com.google.dagger:hilt-compiler:2.51.1")
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+  ksp("com.google.dagger:hilt-compiler:2.51.1")
+  implementation("com.google.dagger:hilt-android:2.51.1")
+  implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-    implementation("com.google.android.gms:play-services-ads:24.0.0")
-    
-    implementation(libs.androidx.adaptive)
-    implementation(libs.androidx.window.core)
+  implementation("com.google.android.gms:play-services-ads:24.0.0")
 
+  implementation(libs.androidx.adaptive)
+  implementation(libs.androidx.window.core)
 
-
-    //Room
-    val room_version = "2.6.1"
-    implementation("androidx.room:room-runtime:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
+  // Room
+  val room_version = "2.6.1"
+  implementation("androidx.room:room-runtime:$room_version")
+  ksp("androidx.room:room-compiler:$room_version")
+  implementation("androidx.room:room-ktx:$room_version")
 }
